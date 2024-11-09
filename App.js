d3070c4169340754832f33639a79c4fdfaf70d64
ChengFangRong <23024324@myrp.edu.sg>
2024-11-09 21:11:49 +0800

    commit

--- App.js
+++ App.js
@@ -4,48 +4,67 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import {ScrollView} from 'react-native';
 import Boat from "./components/boats"
 
+const styles = StyleSheet.create({
+
+  item: {
+    paddingTop: 30,
+    paddingBottom: 20,
+    borderWidth: 2,
+    backgroundColor: 'grey'
+  }
+});
+
 const AllBoats = () => {
   return (
       <View>
-      <ScrollView>
+      <ScrollView style={{padding: 15}}>
+
         <Text></Text>
         <Text></Text>
 
+        <Text style={[ styles.item , {fontWeight: 'bold', textAlign: 'center', fontSize: 24, marginBottom: 20}]}>- BOATS FOR SALE -</Text>
+
+        <View style={{marginBottom: 20}}>
         <Boat
             name="Sea Ray 500 Sundancer"
             description="Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away."
             picture={require('./img/sea_ray.jpg')}
-        />
+        /></View>
 
+        <View style={{marginBottom: 20}}>
         <Boat
             name="Four Winns Horizon 180"
             description="A sporty look and refined details truly set the Horizon 180 above all others."
             picture={require('./img/four_winns.jpg')}
-        />
+        /></View>
 
+        <View style={{marginBottom: 20}}>
         <Boat
             name="Flipper 640 ST"
             description="A modern take on the classic, traditional hardtop and perfect for a family picnic."
             picture={require('./img/flipper.jpg')}
-        />
+        /></View>
 
+        <View style={{marginBottom: 20}}>
         <Boat
             name="Princess V48"
             description="There is the option for an open design with a full-length cockpit and sunroof, or the enclosed deck saloon model, available with the option of a climate controlled interior."
             picture={require('./img/princess.jpg')}
-        />
+        /></View>
 
+        <View style={{marginBottom: 20}}>
         <Boat
             name="Bayliner 175 Bowrider"
             description="Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance."
             picture={require('./img/bayliner.jpg')}
-        />
+        /></View>
 
+        <View style={{marginBottom: 20}}>
         <Boat
             name="Fairline Targa 47"
             description="Stretch out on the large sun bed aft while friends lounge in the generously appointed cockpit."
             picture={require('./img/fairline.jpg')}
-        />
+        /></View>
 
 
 
--- components/boats.js
+++ components/boats.js
@@ -1,21 +1,52 @@
-import {Image, Text, View} from "react-native";
+import {Image, StyleSheet, Text, View} from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome6";
+
+const styles = StyleSheet.create({
+
+    item: {
+        paddingTop: 30,
+        paddingBottom: 20,
+        borderWidth: 2,
+        backgroundColor: 'lightgrey',
+        paddingLeft: 10,
+        paddingRight: 10,
+    },
+
+    title: {
+        backgroundColor: 'grey',
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 24,
+        fontWeight: "bold",
+        padding: 5,
+        marginBottom: 20,
+    },
+
+    description: {
+        paddingLeft: 10,
+        paddingRight: 10,
+        borderWidth: 2,
+        borderColor: 'grey',
+        marginBottom: 20,
+        backgroundColor: 'white',
+    }
+});
 
 
 const Boat = ({name, description, picture}) => {
     return (
-        <View>
+            <View style={[styles.item]}>
             <Text></Text>
-            <Text style={{fontSize: 24, fontWeight: "bold"}}>
+            <Text style={[styles.title]}>
                 <Icon name="sailboat" size={30} color="#000" />
                 {name}
             </Text>
-            <Text>
+            <Text style={[styles.description]}>
                 {description}
             </Text>
-            <Image source={picture} style={{width:400, height:500}}></Image>
+            <Image source={picture} style={{width:355, height:450}}></Image>
             <Text></Text>
-        </View>
+            </View>
     );
 };
 
